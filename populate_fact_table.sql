333ddb75b983df6b5acd5b4241ce7f5b838cf935
Kosiuraih <kosiuraih@gmail.com>
2025-07-14 23:44:33 +0100

    Create populate_fact_table.sql

---
+++ populate_fact_table.sql
@@ -0,0 +1,17 @@
+INSERT INTO fact_sales (
+customerID,
+productID,
+locationID,
+dateID,
+categoryID,
+segmentID,
+orderModeID,
+quantity,
+totalSales,
+totalCost,
+profit,
+discountAmount
+)
+VALUES 
+('C001', 'P001', 'L001', '2024-06-22', 1, 1, 1, 4, 2.99, 0.897, 2.093, 0.24),
+('C002', 'P002', 'L002', '2024-03-15', 2, 2, 2, 16, 7.99, 2.397, 5.593, 0.32);
